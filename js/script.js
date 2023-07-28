5aba223c57e94d7401ed7d9bbde1eea4f6c023f8
Caio-Pfaltzgraff <caio.rllcr@gmail.com>
2023-07-28 18:18:49 -0300

    Promisse.all

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
-async function buscaEndereco() {
-    var consultaCep = await fetch('https://viacep.com.br/ws/26525037/json/')
-    var consultaCepConvertida = await consultaCep.json();
-    console.log(consultaCepConvertida);
+async function buscaEndereco(cep) {
+    try {
+        var consultaCep = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        var consultaCepConvertida = await consultaCep.json();
+        if (consultaCepConvertida.erro) {
+            throw Error("CEP não existente!")
+        }
+        console.log(consultaCepConvertida);
+        return consultaCepConvertida;
+    } catch (erro) {
+        console.log(erro);
+    }
 }
 
-buscaEndereco();
+let ceps = ['01001000', '26525037'];
+let conjuntoCeps = ceps.map(valores => buscaEndereco(valores));
+console.log(conjuntoCeps);
+Promise.all(conjuntoCeps).then(respostas => console.log(respostas))
