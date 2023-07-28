71d6b0d3bbf351a0ed064527ae0afc9ec8624e78
Caio-Pfaltzgraff <caio.rllcr@gmail.com>
2023-07-28 18:53:36 -0300

    preenchendo o formulario

--- js/script.js
+++ js/script.js
@@ -1,18 +1,29 @@
 async function buscaEndereco(cep) {
+    var mensagemErro = document.getElementById('erro');
+    mensagemErro.innerHTML = "";
     try {
         var consultaCep = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
         var consultaCepConvertida = await consultaCep.json();
         if (consultaCepConvertida.erro) {
             throw Error("CEP não existente!")
         }
+        var cidade = document.getElementById('cidade');
+        var logradouro = document.getElementById('endereco');
+        var estado = document.getElementById('estado');
+        var bairro = document.getElementById('bairro');
+        
+        cidade.value = consultaCepConvertida.localidade;
+        logradouro.value = consultaCepConvertida.logradouro;
+        estado.value = consultaCepConvertida.uf;
+        bairro.value = consultaCepConvertida.bairro;
+
         console.log(consultaCepConvertida);
         return consultaCepConvertida;
     } catch (erro) {
+        mensagemErro.innerHTML = `<p>CEP inválido. Tente novamente!</p>`;
         console.log(erro);
     }
 }
 
-let ceps = ['01001000', '26525037'];
-let conjuntoCeps = ceps.map(valores => buscaEndereco(valores));
-console.log(conjuntoCeps);
-Promise.all(conjuntoCeps).then(respostas => console.log(respostas))
+var cep = document.getElementById('cep');
+cep.addEventListener("focusout", () => buscaEndereco(cep.value));
